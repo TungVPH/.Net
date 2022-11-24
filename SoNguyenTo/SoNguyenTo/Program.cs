9e59dcccf4308ac2bed3ccab4986bc6994e88387
TungVPH <TungVPH1@gmail.com>
2022-11-24 13:29:14 +0700

    so nguyen to

---
+++ SoNguyenTo/SoNguyenTo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoNguyenTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 1,n, i = 2;
+            Console.WriteLine("Nhap so can kiem tra n: ");
+            n = int.Parse(Console.ReadLine());
+            for(i=2;i<=n/2;i++)
+            {
+                if(n%i==0)
+                {
+                    k=0;break;
+                }
+                
+            }
+            if(k==1)
+            {
+                Console.WriteLine("So nay la so nguyen to");
+            }
+            else
+            { 
+                Console.WriteLine("So nay khong phai la so nguyen to");
+            }
+            Console.ReadLine();
+        }
+    }
+}
